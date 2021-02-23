72149c2da746167c4b59d90db999d5df9e0d2698
Adrian Grosu <adriangrosu17@gmail.com>
2021-02-23 15:51:32 +0200

    started implementing some opcodes

--- inc/disassembler.h
+++ inc/disassembler.h
@@ -4,7 +4,7 @@
 #include <stdint.h>
 
 uint8_t *LoadBinary(const char *bin, uint16_t *size);
-uint8_t DisassembleInstr(uint8_t *bin, uint16_t offset, uint8_t *clk_cycles);
+uint8_t DisassembleInstr(uint8_t *bin, uint16_t offset);
 
 #endif // _INC_DISASSEMBLER_H_
 
--- src/main.c
+++ src/main.c
@@ -10,18 +10,15 @@ int main(int argc, char *argv[])
     }
     else
     {
-        uint8_t instr_cycles = 0;
         uint16_t bin_size = 0, bytes_disassembled = 0, total_instructions = 0;
-        uint32_t total_cycles = 0;
         uint8_t *bin = LoadBinary(argv[1], &bin_size);
         while(bytes_disassembled < bin_size)
         {
-            bytes_disassembled += DisassembleInstr(bin, bytes_disassembled, &instr_cycles);
-            total_cycles += instr_cycles;
+            bytes_disassembled += DisassembleInstr(bin, bytes_disassembled);
             ++total_instructions;
         }
-        printf("Disassembled a total of %u bytes into %u instructions which take a total of %u cycles to execute\n",
-                bytes_disassembled, total_instructions, total_cycles);
+        printf("Disassembled a total of %u bytes into %u instructions\n",
+                bytes_disassembled, total_instructions);
     }
     return 0;
 }
